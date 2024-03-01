b64867437b2354295d234118d03674ba87ffc085
dehughes21@coe.edu <109769144+dehughes21@users.noreply.github.com>
2024-02-29 19:13:38 -0600

    Added User and Sighting tables

--- ProofOfConcept/admin.py
+++ ProofOfConcept/admin.py
@@ -1,7 +1,11 @@
 from .models import Squirrel
+from .models import User
+from .models import Sighting
 from django.contrib import admin
 
 class SquirrelAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
 
 admin.site.register(Squirrel, SquirrelAdmin)
+admin.site.register(User)
+admin.site.register(Sighting)
--- ProofOfConcept/models.py
+++ ProofOfConcept/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Squirrel(models.Model):
 
@@ -8,3 +9,29 @@ class Squirrel(models.Model):
 
     def __str__(self):
         return self.name
+    
+class User(models.Model):
+    email = models.EmailField('User Email')
+    banned = models.BooleanField(blank=True)
+
+    def __str__(self):
+        return self.email
+    
+class Sighting(models.Model):
+    time = models.DateTimeField('Sighting Time') # SUBJECT TO CHANGE
+
+    #squirrelID = models.FloatField('Squirrel ID', max_length=50)
+    squirrel = models.ForeignKey(Squirrel, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    image = models.ImageField(upload_to='images/')
+    image2 = models.ImageField(upload_to='images/', blank=True)
+    image3 = models.ImageField(upload_to='images/', blank=True)
+    image4 = models.ImageField(upload_to='images/', blank=True)
+    image5 = models.ImageField(upload_to='images/', blank=True)
+    description = models.TextField(blank=True)
+    # Many-to-many relationship for behaviors?
+    
+    def __str__(self):
+        return self.squirrelID + str(self.time)
+    
+
